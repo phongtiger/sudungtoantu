63bc47c365be992f595745a292e65ce8f5fce538
phongtiger <phongken94@gmail.com>
2019-07-31 14:41:25 +0700

    cap nhat

--- ham1.js
+++ ham1.js
@@ -4,5 +4,9 @@ inputWidth = prompt('Nhap chieu rong');
 inputHeight = prompt('Nhap chieu dai');
 let width = parseInt(inputWidth);
 let height = parseInt(inputHeight);
-let area = width*height
+let area = width*height;
+document.write('Chieu rong ='+ inputWidth);
+document.write('<br/>')
+document.write('Chieu dai =' + inputHeight);
+document.write('<br/>')
 document.write("Dien tich la: " + area)
